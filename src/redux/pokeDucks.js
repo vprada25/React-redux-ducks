9a31f478be2099a1d7c9a7246396ce4edc81735f
vprada25 <v.prada@udla.edu.co>
2021-03-12 18:08:28 -0500

    feat(config): add redux and config

---
+++ src/redux/pokeDucks.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+
+//constants
+const datainit = {
+    array: []
+}
+
+const getPokemon = 'pokemon exitoso'
+
+//reducer
+export default function pokeReducer(state = datainit, action) {
+    switch (action.type) {
+        case getPokemon:
+            return { ...state, array: action.payload }
+        default:
+            return state
+
+    }
+}
+
+
+//actions
+export const GetPokeAction = () => async (dispatch, getstate) => {
+    try {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+        dispatch({
+            type: getPokemon,
+            payload: res.data.results
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
